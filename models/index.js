87f795f2cb5ef5c19ec5e488faea2c2046ed1d19
Craig Stroman <cstroman@gmail.com>
2020-10-08 21:24:27 -0400

    Updates

--- models/index.js
+++ models/index.js
@@ -6,15 +6,18 @@ const db = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_USER_PASSWORD;
 
+/*
+  Note: In order to enable logging to the terminal you can replace the logging: false with the following:
+  logging: function (str) {
+    console.log(str);
+  },
+*/
 const sequelize = new Sequelize(db, dbUser, dbPassword, {
   dialect: 'postgres',
   define: {
     underscored: true,
   },
   logging: false,
-  // logging: function (str) {
-  //   console.log(str);
-  // },
 });
 
 const models = {
