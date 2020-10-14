17b14751ad9b55225edec30e7055c02043ecd8a0
Craig Stroman <cstroman@gmail.com>
2020-10-14 11:51:13 -0400

    Code cleanup

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ import { indexPage, loginPage, signupPage, dashboardPage } from '../controllers/
 import { join } from '../controllers/join';
 import { login, logout } from '../controllers/login';
 
-// TODO: Fix this because login is always returning correct user even if incorrect passord is entered.
-// Following: http://www.passportjs.org/docs/authenticate/
-
 const router = new Router();
 
 router.route('/').get(indexPage);
