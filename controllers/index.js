aee32f7db9b98ddab965bde3145ef2eb12336d3e
Craig Stroman <cstroman@gmail.com>
2020-10-15 16:02:24 -0400

    Updating dashboardPage function

--- controllers/index.js
+++ controllers/index.js
@@ -27,6 +27,11 @@ export const signupPage = (req, res) => {
 };
 
 export const dashboardPage = (req, res) => {
+  res.header(
+    'Cache-Control',
+    'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0',
+  );
+
   if (req.isAuthenticated()) {
     res.render('dashboard', {
       title: req.app.locals.title,
